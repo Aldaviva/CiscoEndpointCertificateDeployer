3a8d473326dafce960fa85780cdf35f3cee2aa6e
Ben Hutchison <ben@aldaviva.com>
2025-01-14 15:15:46 -0800

    Return process' exit code from PowerShell script, not PID

--- CiscoEndpointCertificateDeployer/CiscoEndpointCertificateDeployer.ps1
+++ CiscoEndpointCertificateDeployer/CiscoEndpointCertificateDeployer.ps1
@@ -4,4 +4,4 @@ $pfxFilename = $result.ManagedItem.CertificatePath
 
 $process = Start-Process -FilePath ".\CiscoEndpointCertificateDeployer.exe" -ArgumentList $pfxFilename, $endpointHostname, $endpointUsername, $endpointPassword, $applyToServices -Wait -PassThru -RedirectStandardOutput c:\ciscocert-out.txt -RedirectStandardError c:\ciscocert-err.txt
 
-exit $process.Id
+exit $process.ExitCode
